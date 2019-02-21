f671db43769d231fdd782189d240a1f56af03a0a
Ranjeet Chouhan <ranuluhar@gmail.com>
2019-02-22 02:32:21 +0530

    Button Add

--- App.js
+++ App.js
@@ -2,11 +2,21 @@ import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class App extends React.Component {
+        state = {
+          text : "Click Button"
+        }
+
+ onclick(){
+
+      this.setState({text : "Button Clicked"})
+
+ }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Click Button</Text>
-        <Button Text="Click Here"/>
+        <Text>{this.state.text}</Text>
+        <Button title="Click Here" onPress={this.onclick.bind(this)}/>
       </View>
     );
   }
@@ -20,3 +30,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+

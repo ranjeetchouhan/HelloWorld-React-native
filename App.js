52894a28f63c48b55428391c7451e9eb249aa64f
Ranjeet Chouhan <ranuluhar@gmail.com>
2019-02-22 02:15:39 +0530

    no message

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Click Button</Text>
+        <Button Text="Click Here"/>
       </View>
     );
   }
